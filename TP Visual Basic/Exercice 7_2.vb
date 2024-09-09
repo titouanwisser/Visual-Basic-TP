fcb0869b04baa25c96d42a33a376f8ef52de5fe5
Titouan WISSER <titouanwisser@gmail.com>
2024-09-09 19:25:15 +0200

    Exercice 7.2

---
+++ TP Visual Basic/Exercice 7_2.vb
@@ -0,0 +1,23 @@
+﻿Module Exercice_7_2
+    Sub Main()
+        Dim nombre_jours, distance, nombre_jours_essence, distance_essence, nombre_jours_diesel, distance_diesel, montant_essence, montant_diesel As Integer
+        Console.WriteLine("Nombre de jours de location ?")
+        nombre_jours = Console.ReadLine()
+        Console.WriteLine("Distance à parcourir (en kms) ?")
+        distance = Console.ReadLine()
+        nombre_jours_essence = nombre_jours * 30
+        distance_essence = distance * 0.85
+        nombre_jours_diesel = nombre_jours * 35
+        distance_diesel = distance * 0.65
+        montant_essence = nombre_jours_essence + distance_essence
+        montant_diesel = nombre_jours_diesel + distance_diesel
+        If montant_essence < montant_diesel Then
+            Console.WriteLine("Meilleur choix : Essence")
+        ElseIf montant_diesel < montant_essence Then
+            Console.WriteLine("Meilleur choix : Diesel")
+        End If
+        Console.WriteLine("Au revoir !")
+        Console.ReadLine()
+    End Sub
+
+End Module
