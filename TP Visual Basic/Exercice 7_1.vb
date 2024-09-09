847449359e1f8fbb226c027ad4ce6b7464d9ecb5
Titouan WISSER <titouanwisser@gmail.com>
2024-09-09 17:04:22 +0200

    Exercice 7.1

--- TP Visual Basic/Exercice 7_1.vb
+++ TP Visual Basic/Exercice 7_1.vb
@@ -1,3 +1,22 @@
 ﻿Module Exercice_7_1
+    Sub Main()
 
+        Dim montant As Integer
+        Console.WriteLine("Veuillez taper votre montant.")
+        montant = Console.ReadLine()
+        If montant < 2000 Then
+            Console.WriteLine("Pas de remise.")
+            Console.WriteLine("Le montant net est : " + montant.ToString)
+        ElseIf montant > 2000 And montant < 5000 Then
+            Console.WriteLine("Remise de 1%")
+            Console.WriteLine("Le montant net est : " + (montant * 0.99).ToString)
+        ElseIf montant > 5000 Then
+            Console.WriteLine("Remise de 2%")
+            Console.WriteLine("Le montant net est : " + (montant * 0.98).ToString)
+        End If
+        Console.WriteLine("Au revoir !")
+        Console.ReadLine()
+
+
+    End Sub
 End Module
