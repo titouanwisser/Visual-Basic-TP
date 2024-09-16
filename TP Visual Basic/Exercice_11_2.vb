b45c36514477d1cd3ac94ba2071cbaf519d6d07a
Titouan WISSER <titouanwisser@gmail.com>
2024-09-16 14:54:53 +0200

    Exercice 11.2

---
+++ TP Visual Basic/Exercice_11_2.vb
@@ -0,0 +1,26 @@
+﻿Module Exercice_11_2
+    Sub Main()
+        Dim nombre, factorielle As Decimal
+        Do
+            Console.WriteLine("Saisir un nombre >= 0 (-1 pour sortir)")
+            nombre = Console.ReadLine()
+            If nombre < -1 Then
+                Do
+                    Console.WriteLine("Le nombre doit être égal ou supérieur à 0 !")
+                    Console.WriteLine("Saisir un nombre >= 0")
+                    nombre = Console.ReadLine()
+                Loop Until nombre >= 0
+            End If
+            If nombre > 0 Then
+                factorielle = nombre
+                Do
+                    nombre = nombre - 1
+                    factorielle = factorielle * nombre
+                Loop Until nombre = 1
+                Console.WriteLine("La factorielle de ce nombre est : " + factorielle.ToString())
+            End If
+        Loop Until nombre = -1
+        Console.WriteLine("Au revoir !")
+        Console.ReadLine()
+    End Sub
+End Module
