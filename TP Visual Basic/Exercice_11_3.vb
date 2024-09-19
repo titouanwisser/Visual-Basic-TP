b15e477887540f25b763fb809886e13c940194a6
Titouan WISSER <titouanwisser@gmail.com>
2024-09-19 15:28:33 +0200

    Exercice 11.3 - MàJ 11.2 à voir

--- TP Visual Basic/Exercice_11_3.vb
+++ TP Visual Basic/Exercice_11_3.vb
@@ -1,9 +1,27 @@
 ﻿Module Exercice_11_3
+
+    Function DilatationTemps(ByVal v As Decimal, ByVal t As Decimal) As Decimal
+        Dim vitesselumiere As Double
+        vitesselumiere = 300000
+        Return t / Math.Sqrt(1 - (v * v / (vitesselumiere * vitesselumiere)))
+    End Function
+
+    Function ContractionLongueurs(ByVal v As Decimal, ByVal l As Decimal) As Decimal
+        Dim vitesselumiere As Double
+        vitesselumiere = 300000
+        Return l * Math.Sqrt(1 - (v * v / (vitesselumiere * vitesselumiere)))
+    End Function
+
+    Function CompositionVitesses(ByVal vF As Decimal, ByVal vO As Decimal) As Decimal
+        Dim vitesselumiere As Decimal
+        vitesselumiere = 300000
+        Return (vO + vF) / (1 + ((vO * vF) / (vitesselumiere * vitesselumiere)))
+    End Function
+
     Sub Main()
         Dim choix As Integer
-        Dim duree_terre, vitesse_lumiere, vitesse_fusee, duree_fusee As Double
+        Dim vitesse_fusee, vitesse_obus, duree_fusee, taille_fusee As Double
         Dim pi As Decimal
-        vitesse_lumiere = 300000
         pi = 3.1415926535897931
         Console.WriteLine("1. La dilatation du temps")
         Console.WriteLine("2. La contraction des longueurs")
@@ -20,18 +38,23 @@
                     vitesse_fusee = Console.ReadLine()
                     Console.WriteLine("Quelle est la durée écoulée dans la fusée ? (en secondes)")
                     duree_fusee = Console.ReadLine()
-                    duree_terre = duree_fusee / Math.Sqrt(1 - (vitesse_fusee * vitesse_fusee / (vitesse_lumiere * vitesse_lumiere)))
-                    Console.WriteLine("Durée écoulée sur Terre : " + duree_terre.ToString())
+                    Console.WriteLine("Durée écoulée sur Terre : " + DilatationTemps(vitesse_fusee, duree_fusee).ToString())
                     Console.WriteLine(". . . Rappel menu . . .")
 
                 Case 2
-                    Console.WriteLine("Quel est le rayon du cercle ? (Rayon > 0)")
-
+                    Console.WriteLine("Quel est la vitesse de la fusée ? (en km/s)")
+                    vitesse_fusee = Console.ReadLine()
+                    Console.WriteLine("Quelle est la taille de la fusée ? (en kms)")
+                    taille_fusee = Console.ReadLine()
+                    Console.WriteLine("Taille de la fusée vue de la Terre : " + ContractionLongueurs(vitesse_fusee, taille_fusee).ToString())
                     Console.WriteLine(". . . Rappel menu . . .")
 
                 Case 3
-                    Console.WriteLine("Quelle est la longueur du rectangle ? (Longueur > 0)")
-
+                    Console.WriteLine("Quel est la vitesse de la fusée ? (en km/s)")
+                    vitesse_fusee = Console.ReadLine()
+                    Console.WriteLine("Quelle est la vitesse de l'obus, dans le repère de la fusée ? (en kms)")
+                    vitesse_obus = Console.ReadLine()
+                    Console.WriteLine("Vitesse de l'obus par rapport à la Terre : " + CompositionVitesses(vitesse_fusee, vitesse_obus).ToString())
                     Console.WriteLine(". . . Rappel menu . . .")
 
                 Case 4
