3bfc2485d3919371c06aa2bd3a79ebcdf2df6491
Titouan WISSER <titouanwisser@gmail.com>
2024-09-30 17:11:32 +0200

    Exercice 11.4 Update

--- TP Visual Basic/Exercice_11_4.vb
+++ TP Visual Basic/Exercice_11_4.vb
@@ -1,31 +1,40 @@
 ﻿Module Exercice_11_4
 
     Sub main()
+        Dim reponse As String
         Dim choix, compteur, compteur_fois, nombre_etoiles, nombre_espaces, compteur_etoiles, compteur_espaces As Integer
-        Console.WriteLine("Combien de lignes pour votre triangle ? (>= 0)")
-        choix = Console.ReadLine()
-        If choix < 0 Then
-            Do
-                Console.WriteLine("La valeur doit être égale ou supérieure à 0 !")
-                Console.WriteLine("Combien de lignes pour votre triangle ? (>= 0)")
-                choix = Console.ReadLine()
-            Loop Until choix >= 0
-        End If
-        nombre_espaces = choix - 1
-        compteur_etoiles = 1
         Do
-            For compteur_espaces = 1 To nombre_espaces
-                Console.Write(" ")
-            Next
-            For compteur_etoiles = 0 To nombre_etoiles
-                Console.Write("*")
-            Next
-            Console.WriteLine()
-            nombre_etoiles = compteur_etoiles + 1
-            nombre_espaces = nombre_espaces - 1
-            compteur_fois = compteur_fois + 1
-        Loop Until choix = compteur_fois
-        Console.ReadLine()
+            Console.WriteLine("Combien de lignes pour votre triangle ? (>= 0)")
+            choix = Console.ReadLine()
+            If choix < 0 Then
+                Do
+                    Console.WriteLine("La valeur doit être égale ou supérieure à 0 !")
+                    Console.WriteLine("Combien de lignes pour votre triangle ? (>= 0)")
+                    choix = Console.ReadLine()
+                Loop Until choix >= 0
+            End If
+            nombre_espaces = choix - 1
+            compteur_etoiles = 1
+            Do
+                For compteur_espaces = 1 To nombre_espaces
+                    Console.Write(" ")
+                Next
+                For compteur_etoiles = 0 To nombre_etoiles
+                    Console.Write("*")
+                Next
+                Console.WriteLine()
+                nombre_etoiles = compteur_etoiles + 1
+                nombre_espaces = nombre_espaces - 1
+                compteur_fois = compteur_fois + 1
+            Loop Until choix = compteur_fois
+            Console.WriteLine("Voulez-vous un autre triangle ? (O/N)")
+            reponse = Console.ReadLine()
+            nombre_espaces = 0
+            nombre_etoiles = 0
+            compteur_espaces = 0
+            compteur_etoiles = 0
+            compteur_fois = 0
+        Loop Until reponse = "N"
 
     End Sub
 
---
+++ TP Visual Basic/IntroductionTableauxV2.vb
@@ -0,0 +1,43 @@
+﻿Module IntroductionTableauxV2
+
+    Sub Main()
+
+        Const NOMBREDENOTES = 10
+
+        Dim notes(9) As Integer 'Déclaration d'un tableau de 10 entiers
+
+        Dim noNote, somme As Integer
+
+        ' Saisie des notes
+
+        For noNote = 0 To NOMBREDENOTES - 1
+
+            Do
+
+                Console.WriteLine(" Saisir le note n°:" + (noNote + 1).ToString())
+
+                notes(noNote) = Console.ReadLine()
+
+            Loop Until notes(noNote) >= 0 And notes(noNote) <= 20
+
+        Next
+
+        ' Calcul de la moyenne
+
+        somme = 0
+
+        For noNote = 0 To NOMBREDENOTES - 1
+
+            somme = somme + notes(noNote)
+
+        Next
+
+        Console.WriteLine("La moyenne est :" + (somme / NOMBREDENOTES).ToString())
+
+        ' Le tri du tableau en ordre croissant sera fait plus loin
+
+        Console.ReadLine()
+
+    End Sub
+
+End Module
