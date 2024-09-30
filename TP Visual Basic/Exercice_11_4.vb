25067f9c0d6da3e885efe45ea0b1dae1b35fbab8
Titouan WISSER <titouanwisser@gmail.com>
2024-09-30 16:35:27 +0200

    Exercice 11.4

--- TP Visual Basic/Exercice_11_4.vb
+++ TP Visual Basic/Exercice_11_4.vb
@@ -1,8 +1,7 @@
 ﻿Module Exercice_11_4
 
     Sub main()
-        Dim choix, compteur, compteur_fois, compteur_etoiles As Integer
-        compteur_etoiles = 1
+        Dim choix, compteur, compteur_fois, nombre_etoiles, nombre_espaces, compteur_etoiles, compteur_espaces As Integer
         Console.WriteLine("Combien de lignes pour votre triangle ? (>= 0)")
         choix = Console.ReadLine()
         If choix < 0 Then
@@ -12,12 +11,18 @@
                 choix = Console.ReadLine()
             Loop Until choix >= 0
         End If
+        nombre_espaces = choix - 1
+        compteur_etoiles = 1
         Do
-            For compteur = 1 To compteur_etoiles
+            For compteur_espaces = 1 To nombre_espaces
+                Console.Write(" ")
+            Next
+            For compteur_etoiles = 0 To nombre_etoiles
                 Console.Write("*")
             Next
             Console.WriteLine()
-            compteur_etoiles = compteur_etoiles + 2
+            nombre_etoiles = compteur_etoiles + 1
+            nombre_espaces = nombre_espaces - 1
             compteur_fois = compteur_fois + 1
         Loop Until choix = compteur_fois
         Console.ReadLine()
